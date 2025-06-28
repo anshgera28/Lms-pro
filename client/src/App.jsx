4e9fa30b3655719f3a4511f31ac9dbfef3ea0df5
Ansh Gera <anshgera947@gmail.com>
2025-06-28 15:30:18 +0530

    add initial files

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+
+// import './App.css'
+
+// function App() {
+
+//   return (
+//    <>
+
+//    </>
+//   )
+// }
+
+// export default App
+
+
+
+import { Button } from "@/components/ui/button"
+ 
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+ 
+export default App
