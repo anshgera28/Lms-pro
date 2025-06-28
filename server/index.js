576c05c48cda9a7ea866f7fd16a3254ea6a7163d
Ansh Gera <anshgera947@gmail.com>
2025-06-29 00:48:49 +0530

    Add server setup with Express, MongoDB connection, and user model; include .gitignore and package.json for dependencies

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+
+
+dotenv.config({});
+
+connectDB();
+
+const app = express();
+
+const PORT  = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
