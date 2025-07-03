26d28a8f4d8e3273a03e6eed0700418355c21e6b
Ansh Gera <anshgera947@gmail.com>
2025-07-03 18:05:05 +0530

    Add Radix UI components for enhanced UI; update package.json and package-lock.json with new dependencies, including react-avatar and react-dropdown-menu. Refactor App component to include Navbar and adjust Login page layout for improved user experience.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 
 import Login from "./pages/login";
+import Navbar from "./components/ui/Navbar";
  
 function App() {
   return (
     <main className="min-h-screen p-4">
+      <Navbar />
       <Login />
     </main>
   );
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -127,7 +127,7 @@ const Login = () => {
   }, [loginLoading, registerLoading, loginData, registerData, loginError, registerError])
   
   return (
-  <div className="flex w-full items-center justify-center">
+  <div className="flex w-full items-center justify-center mt-20">
       <div className="flex w-full max-w-sm flex-col gap-6">
       <Tabs defaultValue="account">
         <TabsList>
